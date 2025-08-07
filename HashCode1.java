258f28d0914e5088456ed277832b4bc8c1519c5c
Omnarkar31 <narkarom161@gmail.com>
2025-08-07 10:44:46 +0530

    Update HashCode1.java

--- HashCode1.java
+++ HashCode1.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 public class SimpleHash {
 
     static String[] table;
-
-    // Simple hash function: sum of character codes modulo SIZE
     public static int hash(String key) {
         int sum = 0;
         for (int i = 0; i < key.length(); i++) {
@@ -13,14 +11,11 @@ public class SimpleHash {
         return sum % table.length;
     }
 
-    // Insert key into hash table
     public static void insert(String key) {
         int index = hash(key);
         table[index] = key;  // Just place it at the hash index
         System.out.println("Inserted '" + key + "' at index " + index);
     }
-
-    // Display hash table
     public static void display() {
         System.out.println("\nHash Table:");
         for (int i = 0; i < table.length; i++) {
@@ -31,17 +26,15 @@ public class SimpleHash {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Take size of hash table from user
         System.out.print("Enter size of hash table: ");
         int SIZE = sc.nextInt();
-        sc.nextLine(); // Consume newline
+        sc.nextLine(); 
 
-        table = new String[SIZE]; // Initialize table with user-defined size
+        table = new String[SIZE]; 
 
         System.out.print("How many keys to insert? ");
         int n = sc.nextInt();
-        sc.nextLine(); // Consume newline
-
+        sc.nextLine(); 
         for (int i = 0; i < n; i++) {
             System.out.print("Enter key " + (i + 1) + ": ");
             String key = sc.nextLine();
@@ -52,3 +45,4 @@ public class SimpleHash {
         sc.close();
     }
 }
+
